0d12d0ae791ce312461077e3ca5d9556b6ce207a
anton1dev <anton.kuchmasov.dev@gmail.com>
2024-08-07 17:39:11 +0300

    done project GameHub

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,9 +1,36 @@
-import axios from "axios";
-import { rawgApi } from "../vars/rawgApi";
+import axios, { AxiosRequestConfig } from 'axios';
 
-export default axios.create({
-  baseURL: rawgApi.apiUrl,
+export interface FetchResponse<T> {
+  count: number;
+  next: string | null;
+  results: T[];
+}
+
+const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   params: {
-    key: rawgApi.apiKey,
+    key: import.meta.env.VITE_API_KEY,
   },
 });
+
+class APIClient<T> {
+  endpoint: string;
+
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
+
+  getAll = (config: AxiosRequestConfig) => {
+    return axiosInstance
+      .get<FetchResponse<T>>(this.endpoint, config)
+      .then((res) => res.data);
+  };
+
+  get = (id: number | string) => {
+    return axiosInstance
+      .get<T>(this.endpoint + '/' + id)
+      .then((res) => res.data);
+  };
+}
+
+export default APIClient;
